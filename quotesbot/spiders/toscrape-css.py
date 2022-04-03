c48ddb247454462b7e9919ad61107f94e6590126
chriszqyang <chriszqyang@tencent.com>
2022-04-03 10:03:45 +0800

    fix spider name

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-from unicodedata import name
 import scrapy
 
 
 class ToScrapeCSSSpider(scrapy.Spider):
+    name = "toscrape-css"
 
     def __init__(self, **kwargs):
-        self.name = "toscrape-css"
-        super(ToScrapeCSSSpider, self).__init__(name=name, **kwargs)
+        super(ToScrapeCSSSpider, self).__init__(name=ToScrapeCSSSpider.name, **kwargs)
 
         self.start_urls = [
             'http://quotes.toscrape.com/',
@@ -25,4 +24,3 @@ class ToScrapeCSSSpider(scrapy.Spider):
         next_page_url = response.css("li.next > a::attr(href)").extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
-
